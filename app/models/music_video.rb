ec03aac5cd43d4e355612d2942533020f59cbc91
Kevin Huelsmann <kjh311@gmail.com>
2015-07-24 10:09:47 -0700

    Created models

---
+++ app/models/music_video.rb
@@ -0,0 +1,3 @@
+class MusicVideo < ActiveRecord::Base
+  belongs_to :band
+end
---
+++ db/migrate/20150724163751_create_music_videos.rb
@@ -0,0 +1,11 @@
+class CreateMusicVideos < ActiveRecord::Migration
+  def change
+    create_table :music_videos do |t|
+      t.string :title
+      t.string :video_embed_url
+      t.integer :band_id
+
+      t.timestamps null: false
+    end
+  end
+end
